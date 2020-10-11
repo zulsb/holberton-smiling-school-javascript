5003c480fd77a6590916e5d4491941e3f8337e38
zulsb <zulsb2093@gmail.com>
2020-10-11 00:27:20 -0500

    Courses

--- scripts.js
+++ scripts.js
@@ -114,8 +114,44 @@ $(document).ready(function () {
     });
   });
 
+  
   // Courses
+  $(function coursesVideos() {
+    let $coursesVideos = $("#coursesVideos");
+    $.ajax({
+      type: "GET",
+      url: "https://smileschool-api.hbtn.info/courses",
+      success: function (data) {
+        $.each(data.courses, function (i, coursesVideos) {
 
-
+          $("#coursesVideos").append(`
+            <div class="col-sm-6 col-md-3 mb-3">
+              <div class="card mb-3 h-100 bg-transparent border-0">
+                <div class="card border-0">
+                  <img class="card-img-top" src="${coursesVideos.thumb_url}" alt="Card image video 1">
+                  <img class="card-img-overlay mx-auto my-auto" src="images/play.png" alt="Play icon" width="110">
+                </div>
+                <div class="card-body pl-3 pb-0">
+                  <h6 class="card-title font-weight-bolder">${coursesVideos.title}</h6>
+                  <p class="card-text text-muted"><small></small></p>
+                  <div class="d-flex">
+                    <img src="${coursesVideos.author_pic_url}" class="rounded-circle mb-3" width="30" height="30">
+                    <p class="ml-3 text-color font-weight-bold">${coursesVideos.author}</p></div>
+                  </div>
+                  <div class="card-footer border-0 bg-transparent d-flex pl-3">
+                    <img src="images/star_on.png" class=" my-auto ml-1" width="15">
+                    <img src="images/star_on.png" class=" my-auto ml-1" width="15">
+                    <img src="images/star_on.png" class=" my-auto ml-1" width="15">
+                    <img src="images/star_on.png" class=" my-auto ml-1" width="15">
+                    <img src="images/star_off.png" class="my-auto ml-1" width="15">
+                    <p class="ml-auto my-auto text-color"><small class="font-weight-bold">${coursesVideos.duration}</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>`)
+        });
+      }
+    });
+  });
 
 });
